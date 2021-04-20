3559a4709369aeb4dd7e9156493a06e845da39c4
murimija <48052714+murimija@users.noreply.github.com>
2021-04-20 18:58:28 +0300

    Revert "UI работает, начинаю переписывать все методы под str"
    This reverts commit d66901c43b1bedf440f9cbc177df3910d2fe4c3a.

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ class Anim:
     def getName(self):
         return self.name
 
-
     def getProj(self):
         return self.project
 
@@ -38,6 +37,8 @@ wordsOfProject = ["SE", "CE"]
 
 with open('otchet.txt', 'r', encoding='utf-8') as f:
     initial_report = f.read().splitlines()
+
+#print(initial_report)
 
 for i in initial_report:  # Удаляем Новые и Орг
     if arrayInStr(wordsToDelete, i):
@@ -97,6 +98,10 @@ listOfEasy = sorted(listOfEasy, key=operator.attrgetter('project'))
 listOfMedium = sorted(listOfMedium, key=operator.attrgetter('project'))
 listOfHard = sorted(listOfHard, key=operator.attrgetter('project'))
 listOfEdit = sorted(listOfEdit, key=operator.attrgetter('project'))
+
+#printListAnim(listOfEasy)
+#a = listOfMedium[0]
+#currenProject = a.printAnim()
 
 def countEdit(edit):
     if "(1)" in edit:
@@ -161,34 +166,7 @@ def printOtchet():
     print("Правки (", a, ")")
     printProjAndAnim(listOfEdit)
 
+
 printOtchet()
-
-import sys  # sys нужен для передачи argv в QApplication
-from PyQt5 import QtWidgets
-import rpui  # Это наш конвертированный файл дизайна
-
-class ExampleApp(QtWidgets.QMainWindow, rpui.Ui_MainWindow):
-    def __init__(self):
-        # Это здесь нужно для доступа к переменным, методам
-        # и т.д. в файле design.py
-        super().__init__()
-        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        self.createReportButton.clicked.connect(self.createReport)  # Выполнить функцию browse_folder
-        # при нажатии кнопки
-
-    def createReport(self):
-        print("Типа отчет!")
-        self.repOutputLbl.setText("Типа отчет!")
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = ExampleApp()  # Создаём объект класса ExampleApp
-    window.show()  # Показываем окно
-    app.exec_()  # и запускаем приложение
-
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
-
-
 
 
